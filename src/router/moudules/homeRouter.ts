0ca5ddb973a7f8315bc6163b29cce90567f76493
liujiaheng <2429803999@qq.com>
2019-01-29 16:32:51 +0800

    去除路由中的无用代码干扰

--- src/router/moudules/homeRouter.ts
+++ src/router/moudules/homeRouter.ts
@@ -1,27 +1,7 @@
 const homeRouter = {
   path: '/home',
   component: () => import('@/views/Home.vue'),
-  name: 'home',
-  children: [
-    {
-      path: '/selftabpage',
-      name: 'selfTabPage',
-      component: () => import('@/views/SelfTabPage.vue')
-    },
-    {
-      path: '/navmenu',
-      name: 'navmenu',
-      component: () => import('@/views/NavMenu.vue')
-    },
-    {
-      path: '/printdata',
-      name: 'printdata',
-      component: () => import('@/views/PrintData.vue'),
-      meta: {
-        requireAuth: true
-      }
-    }
-  ]
+  name: 'home'
 }
 
 export default homeRouter
